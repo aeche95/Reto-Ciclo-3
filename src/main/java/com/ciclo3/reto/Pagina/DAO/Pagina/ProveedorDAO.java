45f1361457842e962354cab567b426b94946f2c7
Andres <66284241+aeche95@users.noreply.github.com>
2021-09-21 20:41:40 -0500

    Add html code in resources and classes for proveedor

---
+++ src/main/java/com/ciclo3/reto/Pagina/DAO/Pagina/ProveedorDAO.java
@@ -0,0 +1,5 @@
+package com.ciclo3.reto.Pagina.DAO.Pagina;
+
+public class ProveedorDAO {
+
+}
---
+++ src/main/java/com/ciclo3/reto/Pagina/DTO/Pagina/ProveedorVO.java
@@ -0,0 +1,83 @@
+package com.ciclo3.reto.Pagina.DTO.Pagina;
+
+public class ProveedorVO {
+	
+	private Integer idProveedor;
+	private Integer NITProveedor;
+	private String  NombreProveedor;
+	private String DireccionProveedor;
+	private Integer TelefonoProveedor;
+	private String CiudadProveedor;
+	/**
+	 * @return the idProveedor
+	 */
+	public Integer getIdProveedor() {
+		return idProveedor;
+	}
+	/**
+	 * @param idProveedor the idProveedor to set
+	 */
+	public void setIdProveedor(Integer idProveedor) {
+		this.idProveedor = idProveedor;
+	}
+	/**
+	 * @return the nITProveedor
+	 */
+	public Integer getNITProveedor() {
+		return NITProveedor;
+	}
+	/**
+	 * @param nITProveedor the nITProveedor to set
+	 */
+	public void setNITProveedor(Integer nITProveedor) {
+		NITProveedor = nITProveedor;
+	}
+	/**
+	 * @return the nombreProveedor
+	 */
+	public String getNombreProveedor() {
+		return NombreProveedor;
+	}
+	/**
+	 * @param nombreProveedor the nombreProveedor to set
+	 */
+	public void setNombreProveedor(String nombreProveedor) {
+		NombreProveedor = nombreProveedor;
+	}
+	/**
+	 * @return the direccionProveedor
+	 */
+	public String getDireccionProveedor() {
+		return DireccionProveedor;
+	}
+	/**
+	 * @param direccionProveedor the direccionProveedor to set
+	 */
+	public void setDireccionProveedor(String direccionProveedor) {
+		DireccionProveedor = direccionProveedor;
+	}
+	/**
+	 * @return the telefonoProveedor
+	 */
+	public Integer getTelefonoProveedor() {
+		return TelefonoProveedor;
+	}
+	/**
+	 * @param telefonoProveedor the telefonoProveedor to set
+	 */
+	public void setTelefonoProveedor(Integer telefonoProveedor) {
+		TelefonoProveedor = telefonoProveedor;
+	}
+	/**
+	 * @return the ciudadProveedor
+	 */
+	public String getCiudadProveedor() {
+		return CiudadProveedor;
+	}
+	/**
+	 * @param ciudadProveedor the ciudadProveedor to set
+	 */
+	public void setCiudadProveedor(String ciudadProveedor) {
+		CiudadProveedor = ciudadProveedor;
+	}
+}
