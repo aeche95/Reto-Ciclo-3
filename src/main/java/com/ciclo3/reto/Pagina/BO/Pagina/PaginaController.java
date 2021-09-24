9cc7df6a19d50f733aa61dc787301aa500b024cc
Andres <66284241+aeche95@users.noreply.github.com>
2021-09-23 20:38:58 -0500

    Set the login html page to return as the main page, now uses @controller instead of @restcontroller

--- src/main/java/com/ciclo3/reto/Pagina/BO/Pagina/PaginaController.java
+++ src/main/java/com/ciclo3/reto/Pagina/BO/Pagina/PaginaController.java
@@ -1,14 +1,15 @@
 package com.ciclo3.reto.Pagina.BO.Pagina;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class PaginaController {
 	@GetMapping("")
 	public String Principal()
 	{
-		return "Pagina principal";
+		return "login.html";
 		
 	}
 
