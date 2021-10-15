892f2d0c691c27515ca6150b236b4c2649393c85
Rikrdo1987 <91297086+Rikrdo1987@users.noreply.github.com>
2021-10-15 16:11:08 -0500

    Create ProductoController.java

---
+++ src/main/java/com/ciclo3/reto/Controllers/ProductoController.java
@@ -0,0 +1,59 @@
+package com.ciclo3.reto.Controllers;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ciclo3.reto.Modelos.ProductoModel;
+import com.ciclo3.reto.Services.ProductoService;
+
+
+@RestController
+@RequestMapping("/productos")
+public class ProductoController {
+
+	@Autowired
+	ProductoService productoService;
+	
+	@GetMapping
+	public ArrayList<ProductoModel> obtenerProductos()
+	{
+		return productoService.ObtenerProductos();
+	}
+
+	@PostMapping
+	public ProductoModel CrearProducto(@RequestBody ProductoModel producto)
+	{
+		return productoService.GuardarProducto(producto);
+	}
+	
+	@GetMapping(path="{id}")
+	public Optional<ProductoModel> ObtenerPorId(@PathVariable("id") Long Id)
+	{
+		return productoService.ObtenerPorId(Id);
+	}
+	
+	@DeleteMapping(path="{id}")
+	public String EliminarPorId(@PathVariable("id") Long Id)
+	{
+		boolean Eliminado = productoService.EliminarProducto(Id);
+		
+		if(Eliminado)
+		{
+			return "Producto eliminado";
+		}
+		else
+		{
+			return "Error al eliminar producto";
+		}
+		
+	}
+}
